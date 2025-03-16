0ff78f97da12a6c66efe53acf293c16718f34bcb
sonsy__20 <20200927@sungshin.ac.kr>
2025-03-16 16:24:39 +0900

    프로그래머스 스택/큐 프로세스

---
+++ src/StackQueue/PG_Lv2_프로세스.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+   public int solution(int[] priorities, int location) {
+      int answer = 0;
+      boolean[] visited = new boolean[priorities.length];
+      Deque<int[]> q = new ArrayDeque<>();
+      PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+      for(int i =0;i<priorities.length;i++){
+         q.offer(new int[]{priorities[i],i});
+         pq.offer(priorities[i]);
+      }
+
+      while(true){
+         int[] p = q.poll();
+
+         if(p[0]<pq.peek()){
+            q.offer(new int[]{p[0],p[1]});
+         }else{
+            answer++;
+            if(p[1]==location) break;
+            pq.poll();
+         }
+      }
+      return answer;
+   }
+}

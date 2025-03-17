4a8036b6b7e8d5f2498841a7496ac509b775f02b
seyoung <20200927@sungshin.ac.kr>
2025-03-17 21:27:32 +0900

    PG Lv1 K번째 수 풀이

---
+++ src/Sort/PG_Lv1_K번째수.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+//+2점
+class Solution {
+   public List<Integer> solution(int[] array, int[][] commands) {
+      List<Integer> cut;
+      List<Integer> answer = new ArrayList<>();
+      for(int i=0;i<commands.length;i++){
+         cut = new ArrayList<>();
+         for(int t =commands[i][0];t<=commands[i][1];t++){
+            cut.add(array[t-1]);
+         }
+         Collections.sort(cut);
+         answer.add(cut.get(commands[i][2]-1));
+
+      }
+      return answer;
+   }
+}
+
+
+//최적화한 코드
+class Solution{
+   public int[] solution(int[] array, int[][] commands) {
+      int[] answer = new int[commands.length];
+
+      for(int i =0;i<commands.length;i++){
+         int start = commands[i][0]-1;
+         int end = commands[i][1];
+         int k = commands[i][2]-1;
+
+         int[] cut = Arrays.copyOfRange(array, start, end);
+         Arrays.sort(cut);
+
+         answer[i]= cut[k];
+      }
+      return answer;
+   }
+}

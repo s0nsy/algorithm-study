594c9ce9e1df6ba80c4c964cb6d7f976a938a601
seyoung <20200927@sungshin.ac.kr>
2025-04-26 22:20:57 +0900

    PG Lv2 주식가격 풀이

---
+++ src/StackQueue/PG_Lv2_주식가격.java
@@ -0,0 +1,21 @@
+import java.util.*;
+//+4점
+class Solution {
+   public int[] solution(int[] prices) {
+      List<Integer> list = new ArrayList<>();
+      for(int i = 0;i<prices.length;i++){
+         int cnt =0;
+         for(int j =i+1;j<prices.length;j++){
+            cnt++;
+            if(prices[i]>prices[j])
+               break;
+         }
+         list.add(cnt);
+      }
+      int[] answer = new int[list.size()];
+      for(int i =0;i<answer.length;i++){
+         answer[i]=list.get(i);
+      }
+      return answer;
+   }
+}

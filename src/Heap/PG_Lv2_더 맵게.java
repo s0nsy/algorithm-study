2a0199fe6e4b7a7c9feea190229a9a82f21b424b
sonsy__20 <20200927@sungshin.ac.kr>
2025-06-05 19:47:52 +0900

    PG Lv2 더 맵게 풀이

--- src/Heap/PG_Lv2_더 맵게.java
+++ src/Heap/PG_Lv2_더 맵게.java
@@ -8,10 +8,10 @@ class Solution {
          pq.offer(i);
 
       while(pq.size()>1&&pq.peek()<K){
-         int first = pq.poll();
+         int first = pq.poll(); // 우선 순위 큐를 사용하여 항상 가장 맵지 않은 두 음식 꺼내기
          int second = pq.poll();
          int newScov= first+ second*2;
-         pq.offer(newScov);
+         pq.offer(newScov); // 새로 계산된 스코빌 지수를 다시 큐에 넣기
          answer++;
       }
       return pq.peek() >=K?answer:-1;
